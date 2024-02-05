dc0c6b909374358d74d30a5f66f374546b1b331e
Endri Kasa <94308935+Eksodia@users.noreply.github.com>
2024-02-05 12:57:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from exercises.Funksionet import *
+from exercises.Struktura_lineare import *
+from exercises.Strukturat_Ciklike import *
+from exercises.Strukturat_Kushtezuese import *
+from exercises.Vektorët import *
+
